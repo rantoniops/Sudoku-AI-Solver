dfb315987ce8c9e0fa796ffa049bf24f68de3ad0
Antonio Perez <rantoniops@gmail.com>
2017-07-05 13:13:22 -0500

    finished project

---
+++ sudoku-1267/README.md
@@ -0,0 +1,43 @@
+# Artificial Intelligence Nanodegree
+## Introductory Project: Diagonal Sudoku Solver
+
+# Question 1 (Naked Twins)
+Q: How do we use constraint propagation to solve the naked twins problem?  
+A: It actually works a lot like the "eliminate" function, I ended up recycling a lot of its code. The key difference in the naked_twins constraint is that the function acts on each unit in isolation instead of acting on all 20 peers at once. What I did was to first to loop through all units and separate all values which were 2 in length and duplicated, then I looped through the unit again and deleted the naked_twins numbers.
+
+# Question 2 (Diagonal Sudoku)
+Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
+A: At first I wrote a function that would check if they were duplicates in the diagonals but I didn't end up using it because the solution ended up being way simpler than that, I just had to add the 2 extra lists of boxes corresponding to the diagonals into the original unitslist and the code automatically accommodated them.
+
+### Install
+
+This project requires **Python 3**.
+
+We recommend students install [Anaconda](https://www.continuum.io/downloads), a pre-packaged Python distribution that contains all of the necessary libraries and software for this project.
+Please try using the environment we provided in the Anaconda lesson of the Nanodegree.
+
+##### Optional: Pygame
+
+Optionally, you can also install pygame if you want to see your visualization. If you've followed our instructions for setting up our conda environment, you should be all set.
+
+If not, please see how to download pygame [here](http://www.pygame.org/download.shtml).
+
+### Code
+
+* `solution.py` - You'll fill this in as part of your solution.
+* `solution_test.py` - Do not modify this. You can test your solution by running `python solution_test.py`.
+* `PySudoku.py` - Do not modify this. This is code for visualizing your solution.
+* `visualize.py` - Do not modify this. This is code for visualizing your solution.
+
+### Visualizing
+
+To visualize your solution, please only assign values to the values_dict using the ```assign_values``` function provided in solution.py
+
+### Submission
+Before submitting your solution to a reviewer, you are required to submit your project to Udacity's Project Assistant, which will provide some initial feedback.  
+
+The setup is simple.  If you have not installed the client tool already, then you may do so with the command `pip install udacity-pa`.  
+
+To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login for alternate login instructions.
+
+This process will create a zipfile in your top-level directory named sudoku-<id>.zip.  This is the file that you should submit to the Udacity reviews system.
---
+++ sudoku-1267/solution.py
@@ -0,0 +1,257 @@
+
+
+# HI REVIEWER!
+# I BROUGHT IN ALL THE CODE WE USED IN THE UDACITY LECTURES, THE ONLY NEW CODE I WROTE IS THE NAKED_TWINS FUNCTION AND THE MODIFICATION
+# OF THE UNITSLIST IN ORDER TO ACCOMMODATE THE DIAGONALS
+
+
+# this is a library I use for checking duplicates
+import collections
+
+assignments = []
+
+# HERE I CREATE THE 2 NEW LISTS FOR THE DIAGONALS
+diagonals = [ [ 'A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9' ] , [ 'A9', 'B8', 'C7', 'D6', 'E5', 'F4', 'G3', 'H2', 'I1' ] ]
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [s+t for s in A for t in B]
+
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+# HERE I ADD THE DIAGONALS TO THE UNITSLIST SO THEY ACT AS CONSTRAINTS IN THE REST OF THE CODE
+unitlist = row_units + column_units + square_units + diagonals
+
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+
+def assign_value(values, box, value):
+    """
+    Please use this function to update your values dictionary!
+    Assigns a value to a given box. If it updates the board record it.
+    """
+    # Don't waste memory appending actions that don't actually change any values
+    if values[box] == value:
+        return values
+
+    values[box] = value
+    if len(value) == 1:
+        assignments.append(values.copy())
+    return values
+
+
+def naked_twins(values):
+    """Eliminate values using the naked twins strategy.
+    Args:
+        values(dict): a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns:
+        the values dictionary with the naked twins eliminated from peers.
+    """
+    # THIS FUNCTION ACTUALLY WORKS OUT A LOT LIKE THE ELIMINATE FUNCTION
+
+    # FIRST I LOOP THROUGH ALL THE UNITS
+    for unit in unitlist:
+        # I SEPARATE ALL BOXES THAT HAVE 2 DIGITS IN THEIR VALUES
+        length_2_boxes = [box for box in unit if len(values[box]) == 2]
+        # THEN I START SEARCHNG FOR DUPLICATES
+        if len(length_2_boxes) > 1: #FIRST I CHECK IF THERES MORE THAN ONE BOX WITH 2 DIGITS
+            boxes_dict = {}
+            # BUT BEFORE I MAKE SURE TO MAP THE BOXES ORIGINAL VALUES BACK INTO THEM
+            for box in length_2_boxes:
+                boxes_dict[box] = values[box]
+            # I EXTRACT THE VALUES WHICH ARE DUPLICATES
+            duplicates = [item for item, count in collections.Counter(boxes_dict.values()).items() if count > 1]
+            for duplicate in duplicates:
+                # INSTEAD OF GOING THORUGH ALL THE 20 PEERS, I ONLY SEARCH WITHIN ONE UNIT
+                for box in unit:
+                    for digit in duplicate:
+                        # IF THE DIGITS ARE IN A NON-NAKED_TWIN BOX
+                        if digit in values[box] and duplicate != values[box]:
+                            # I DELETE THE NUMBERS THEY HAVE FROM THE NAKED_TWINS
+                            values[box] = values[box].replace(digit,'')
+    return values
+
+
+# I ENDED UP NOT HAVING TO USE THIS FUNCTION
+# def check_diagonals(values):
+#     diagonals_dicts = []
+#     for diagonal in diagonals:
+#         diagonals_dict = {}
+#         for box in diagonal:
+#             diagonals_dict[box] = values[box]
+#         diagonals_dicts.append(diagonals_dict)
+#     # print('diagonal_dicts are {}'.format(diagonals_dicts))
+#     for diagonal_dictionary in diagonals_dicts:
+#         duplicates = [item for item, count in collections.Counter(diagonal_dictionary.values()).items() if count > 1]
+#         # print('diagonal duplicates are {}'.format(duplicates))
+#         if len(duplicates) > 0:
+#             return False
+#         else:
+#             return True
+
+
+
+
+
+def grid_values(grid):
+    """
+    Convert grid into a dict of {square: char} with '123456789' for empties.
+    Args:
+        grid(string) - A grid in string form.
+    Returns:
+        A grid in dictionary form
+            Keys: The boxes, e.g., 'A1'
+            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
+    """
+    chars = []
+    digits = '123456789'
+    for c in grid:
+        if c in digits:
+            chars.append(c)
+        if c == '.':
+            chars.append(digits)
+    assert len(chars) == 81
+    return dict(zip(boxes, chars))
+
+
+def display(values):
+    """
+    Display the values as a 2-D grid.
+    Args:
+        values(dict): The sudoku in dictionary form
+    """
+    width = 1 + max( len(values[s]) for s in boxes )
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
+
+
+def eliminate(values):
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            # values[peer] = values[peer].replace(digit,'')
+            replaced_box = values[peer].replace(digit,'')
+            assign_value(values, peer, replaced_box)
+    return values
+
+
+def only_choice(values):
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                # values[dplaces[0]] = digit
+                assign_value(values, dplaces[0], digit)
+    return values
+
+
+def reduce_puzzle(values):
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+
+def search(values):
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes):
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n, w = min( ( len(values[s]), s ) for s in boxes if len(values[s]) > 1 )
+
+    # w = None
+    # for s in boxes:
+    #     length = len(values[s])
+    #     if length > 1:
+    #         # print('length is {}'.format(length))
+    #         w = s
+    #         # print('first one length > 1 is {}'.format(w))
+    #         break
+    # for s in boxes:
+    #     length = len(values[s])
+    #     if length > 1:
+    #         if s < w:
+    #             w = s
+    # print('n is {}'.format(n))
+    # print('w is {}'.format(w))
+
+    # Now use recurrence to solve each one of the resulting sudokus, and
+    for value in values[w]:
+        new_sudoku = values.copy()
+        new_sudoku[w] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+
+
+
+
+def solve(grid):
+    """
+    Find the solution to a Sudoku grid.
+    Args:
+        grid(string): a string representing a sudoku grid.
+            Example: '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    Returns:
+        The dictionary representation of the final sudoku grid. False if no solution exists.
+    """
+    values = grid_values(grid)
+    final_values = search(values)
+    return final_values
+
+
+# solved_diag_sudoku = {'G7': '8', 'G6': '9', 'G5': '7', 'G4': '3', 'G3': '2', 'G2': '4', 'G1': '6', 'G9': '5',
+#                       'G8': '1', 'C9': '6', 'C8': '7', 'C3': '1', 'C2': '9', 'C1': '4', 'C7': '5', 'C6': '3',
+#                       'C5': '2', 'C4': '8', 'E5': '9', 'E4': '1', 'F1': '1', 'F2': '2', 'F3': '9', 'F4': '6',
+#                       'F5': '5', 'F6': '7', 'F7': '4', 'F8': '3', 'F9': '8', 'B4': '7', 'B5': '1', 'B6': '6',
+#                       'B7': '2', 'B1': '8', 'B2': '5', 'B3': '3', 'B8': '4', 'B9': '9', 'I9': '3', 'I8': '2',
+#                       'I1': '7', 'I3': '8', 'I2': '1', 'I5': '6', 'I4': '5', 'I7': '9', 'I6': '4', 'A1': '2',
+#                       'A3': '7', 'A2': '6', 'E9': '7', 'A4': '9', 'A7': '3', 'A6': '5', 'A9': '1', 'A8': '8',
+#                       'E7': '6', 'E6': '2', 'E1': '3', 'E3': '4', 'E2': '8', 'E8': '5', 'A5': '4', 'H8': '6',
+#                       'H9': '4', 'H2': '3', 'H3': '5', 'H1': '9', 'H6': '1', 'H7': '7', 'H4': '2', 'H5': '8',
+#                       'D8': '9', 'D9': '2', 'D6': '8', 'D7': '1', 'D4': '4', 'D5': '3', 'D2': '7', 'D3': '6',
+#                       'D1': '5'}
+
+
+
+if __name__ == '__main__':
+    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    display(solve(diag_sudoku_grid))
+
+    print('NEW ONE FOR TESTING BELOW')
+
+    other_diagonal_grid = '9.1....8.8.5.7..4.2.4....6...7......5..............83.3..6......9................'
+    display(solve(other_diagonal_grid))
+
+    # display(solved_diag_sudoku)
+
+    try:
+        from visualize import visualize_assignments
+        visualize_assignments(assignments)
+
+    except SystemExit:
+        pass
+    except:
+        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
